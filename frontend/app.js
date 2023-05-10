f26191f7db52b2d086083fa92fe3fde15670e136
Emil Salagean <emil.salagean@amadeus.com>
2023-05-10 11:23:47 +0200

    working locally

--- frontend/app.js
+++ frontend/app.js
@@ -1,23 +1,50 @@
-document.getElementById('call-function').addEventListener('click', async () => {
-    const functionKey = 'SNLWnbx7O3ezNHJOgEicu4u9PzrRw6g06At13zcqBYWyAzFuAewBtw==';
-    const apiUrl = `https://app-azure-function-examples-230509115615.azurewebsites.net/api/HttpTriggerJava?code=${functionKey}&name=Emil`;
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+const functionKey = 'SNLWnbx7O3ezNHJOgEicu4u9PzrRw6g06At13zcqBYWyAzFuAewBtw==';
+const base_url = `http://localhost:41413/api/HttpTriggerJava?code=${functionKey}`
+//const base_url = `https://app-azure-function-examples-230509115615.azurewebsites.net/api/HttpTriggerJava?code=${functionKey}`
 
-        if (response.ok) {
-            const data = await response.text();
-            alert(`Function response: ${data}`);
-        } else {
-            alert(`Error: ${response.status} ${response.statusText}`);
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        alert(`Error: ${error.message}`);
+let firstOption = null;
+
+// Handle clicks on the first set of images
+page1.addEventListener("click", (event) => {
+    if (event.target.tagName === "IMG") {
+        firstOption = event.target.dataset.option;
+        page1.classList.add("hidden");
+        page2.classList.remove("hidden");
+        page2.classList.add("image-container");
     }
 });
+
+
+// Handle clicks on the second set of images
+page2.addEventListener("click", async (event) => {
+    if (event.target.tagName === "IMG") {
+        const secondOption = event.target.dataset.option;
+
+
+        const apiUrl = base_url+ `&character=${firstOption}&location=${secondOption}`
+
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.text();
+                alert(`Function response: ${data}`);
+            } else {
+                alert(`Error: ${response.status} ${response.statusText}`);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert(`Error: ${error.message}`);
+        }
+    }
+});
+
--- src/main/java/com/esalagea/sandbox/functions/HttpTriggerJava.java
+++ src/main/java/com/esalagea/sandbox/functions/HttpTriggerJava.java
@@ -20,13 +20,17 @@ public class HttpTriggerJava {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+//        String query = request.getQueryParameters().get("name");
+//        String name = request.getBody().orElse(query);
 
-        if (name == null) {
+        String character = request.getQueryParameters().get("character");
+        String location = request.getQueryParameters().get("location");
+
+
+        if (character == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name + "[v1]").build();
+            return request.createResponseBuilder(HttpStatus.OK).body("Here is a story about a " + character + " in a " + location).build();
         }
     }
 }
